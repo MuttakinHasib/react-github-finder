0f769a15892a91caa74ec0c5f78a4fc5d3c7d9e5
MuttakinHasib <hasibmolla28@gmail.com>
2020-03-25 23:44:46 +0600

    Updates

---
+++ precache-manifest.9cb2113ff35d712786e35fe0ca00e5c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ed9133bb04bb76231578ef3001d359",
+    "url": "/react-github-finder/index.html"
+  },
+  {
+    "revision": "a4dfd939352303bd85ef",
+    "url": "/react-github-finder/static/css/main.e1399b3d.chunk.css"
+  },
+  {
+    "revision": "e45ed690536c57bc5b0a",
+    "url": "/react-github-finder/static/js/2.082576f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-github-finder/static/js/2.082576f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4dfd939352303bd85ef",
+    "url": "/react-github-finder/static/js/main.f554fd7d.chunk.js"
+  },
+  {
+    "revision": "c6f945943b9153eb715f",
+    "url": "/react-github-finder/static/js/runtime-main.e1df7df4.js"
+  },
+  {
+    "revision": "4c14021095fd20edc900e2e9b91d318e",
+    "url": "/react-github-finder/static/media/spinner.4c140210.gif"
+  }
+]);
